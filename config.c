ccb00b1e5e7f25d4859f4190030a54de4407f61d
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-04-29 07:46:01 +0000

    fsmonitor: only enable it in non-bare repositories
    The entire point of the FSMonitor is to monitor the worktree changes in
    a more efficient manner than `lstat()`ing all worktree files every time
    we refresh the index.
    
    But if there is no worktree, FSMonitor has nothing to monitor.
    
    So let's ignore if an FSMonitor is configured (e.g. in `~/.gitconfig`)
    and we're running in a repository without worktree.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -2515,6 +2515,12 @@ int git_config_get_max_percent_split_change(void)
 
 int repo_config_get_fsmonitor(struct repository *r)
 {
+	if (!r->worktree) {
+		/* FSMonitor makes no sense in bare repositories */
+		core_fsmonitor = 0;
+		return 1;
+	}
+
 	if (r->settings.use_builtin_fsmonitor > 0) {
 		core_fsmonitor = "(built-in daemon)";
 		return 1;
